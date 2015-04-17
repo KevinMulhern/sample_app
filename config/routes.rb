2a76b0b7844cc1b3ad0b202f85f6c2b6d5ea579b
KevinMulhern <kevinmulhern23@gmail.com>
2015-04-17 10:49:43 +0100

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   delete  'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets, only:     [:new, :create, :edit, :update]
+  resources :microposts, only:          [:create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
